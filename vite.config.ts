76e4e16a2bbae4f56b0d253601471fa63b8e4f26
Paula Santamaría <m.paula.santamaria@gmail.com>
2024-07-28 22:38:23 -0300

    lint fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/phaser3-ts-vite-eslint/'
-})
+    base: '/phaser3-ts-vite-eslint/'
+});
